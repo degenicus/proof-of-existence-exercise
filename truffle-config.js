d378b6c93e87251969ea48a20b853aaaada873ef
Magnus Brantheim <m.brantheim@gmail.com>
2021-09-11 21:01:59 +0200

    Added deploy config for rinkeby using infura

--- truffle-config.js
+++ truffle-config.js
@@ -18,10 +18,10 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+const infuraURL = fs.readFileSync(".infura").toString().trim();
 
 module.exports = {
   /**
@@ -45,6 +45,11 @@ module.exports = {
      host: 'localhost',     // Localhost (default: none)
      port: 8545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, infuraURL),
+      network_id: 4,          // Rinkeby's network id
+      gas: 5500000,        
     },
     // Another network with more advanced options...
     // advanced: {
